62b3d5b12c4bb094f9251d440934feb9006c1544
Arthur Lelis Uchoa <arthurlelisuchoa32@gmail.com>
2025-11-30 18:29:45 -0300

    Update db.py

--- core/db.py
+++ core/db.py
@@ -26,7 +26,7 @@ async def get_session():
         yield session
 
 @event.listens_for(engine, "do_connect")
-def _set_sqlitye_pragma(dbapi_conn, conn_record):
+def _set_sqlite_pragma(dbapi_conn, conn_record):
     if isinstance(dbapi_conn, sqlite3.Connection):
         dbapi_conn.execute("PRAGMA journal_mode=WAL;")
         dbapi_conn.execute("PRAGMA synchronous=NORMAL;")
